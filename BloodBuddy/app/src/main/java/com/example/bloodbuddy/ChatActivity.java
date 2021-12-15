ce61438de417ef06f38f8d243db23bba74921df3
ayushwant <ayushwant912@gmail.com>
2021-12-15 23:47:32 +0530

    uploading image in storage

--- BloodBuddy/app/src/main/java/com/example/bloodbuddy/ChatActivity.java
+++ BloodBuddy/app/src/main/java/com/example/bloodbuddy/ChatActivity.java
@@ -1,23 +1,33 @@
 package com.example.bloodbuddy;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.bloodbuddy.Adapers.MessagesAdaptor;
 import com.example.bloodbuddy.databinding.ActivityChatBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 
@@ -31,6 +41,7 @@ public class ChatActivity extends AppCompatActivity {
     FirebaseDatabase database;
     FirebaseAuth auth;
     FirebaseUser currentUser;
+    FirebaseStorage storage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +59,7 @@ public class ChatActivity extends AppCompatActivity {
         // get the current user and credentials
         auth = FirebaseAuth.getInstance();
         currentUser = auth.getCurrentUser();
+        storage = FirebaseStorage.getInstance();
 
         String name = getIntent().getStringExtra("name");
         String receiverUid = getIntent().getStringExtra("uid");
@@ -127,8 +139,48 @@ public class ChatActivity extends AppCompatActivity {
             }
         });
 
+        binding.attachmentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+//                intent.setType("*/*"); // for every type of file
+                startActivityForResult(intent, 25);
+
+            }
+        });
+
         getSupportActionBar().setTitle(name);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==25 && data!=null && data.getData()!=null)
+        {
+            Uri selectedImage = data.getData();
+            Calendar calendar = Calendar.getInstance();
+            StorageReference reference = storage.getReference().child("chats").child(calendar.getTimeInMillis() +"");
+
+            reference.putFile(selectedImage).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    if(task.isSuccessful())
+                    {
+                        reference.getDownloadUrl().addOnSuccessListener(uri -> {
+                            String filepath = uri.toString();
+                            Toast.makeText(ChatActivity.this, filepath, Toast.LENGTH_SHORT).show();
+
+                        });
+                    }
+                }
+            });
+
+        }
 
     }
 
