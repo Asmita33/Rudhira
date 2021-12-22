9ef53b1eca5aa37f42a83edf9235ba4c2cbcd64e
Anchal Yadav <anchalyadav3420@gmail.com>
2021-12-22 22:39:20 +0530

    admin Notification added

--- BloodBuddy/app/src/main/java/com/example/bloodbuddy/bloodRequest/RaiseRequest.java
+++ BloodBuddy/app/src/main/java/com/example/bloodbuddy/bloodRequest/RaiseRequest.java
@@ -3,10 +3,15 @@ package com.example.bloodbuddy.bloodRequest;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
@@ -110,11 +115,38 @@ public class RaiseRequest extends AppCompatActivity {
                     }
                 });
 
-
+                //to send notification to the admin
+                if(ContextCompat.checkSelfPermission(RaiseRequest.this, Manifest.permission.SEND_SMS)
+                        == PackageManager.PERMISSION_GRANTED){
+                    //when permission is granted, create method
+                    sendMessage();
+                }
+                else{
+                    ActivityCompat.requestPermissions(RaiseRequest.this
+                            , new String[] {Manifest.permission.SEND_SMS}
+                            , 100);
+                }
 
             }
         });
 
+    }
+
+    private void sendMessage() {
+        String name= patient.getName();
+
+        if(!name.equals("")){
+            SmsManager smsManager= SmsManager.getDefault();
+            String toSend= "A new User "+name+" registered to Raise a request";
+            //send text message
+            smsManager.sendTextMessage("8317065337", null, toSend, null, null);
+
+            Toast.makeText(getApplicationContext(), "Notification has been sent to admin! You will be verified soon!!", Toast.LENGTH_LONG).show();
+
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Enter Message first..", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
